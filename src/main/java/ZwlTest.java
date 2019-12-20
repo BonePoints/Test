295824390631bf69f65e19aea94932bb3f00dda8
zwl <755702>
2019-12-20 09:10:09 +0800

    曾伟丽添加一个脚本

---
+++ src/main/java/ZwlTest.java
@@ -0,0 +1,11 @@
+public class ZwlTest {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
